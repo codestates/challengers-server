ec118607b1dd8f4010d95e98dcc92a1bd6e2ce08
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-01 12:03:46 +0900

    DB data seed upload

---
+++ seeders/20210430200425-like.js
@@ -0,0 +1,55 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "likes",
+      [
+        {
+          user_id: 1,
+          challenge_id: 1,
+        },
+        {
+          user_id: 1,
+          challenge_id: 2,
+        },
+        {
+          user_id: 1,
+          challenge_id: 3,
+        },
+        {
+          user_id: 1,
+          challenge_id: 4,
+        },
+        {
+          user_id: 2,
+          challenge_id: 1,
+        },
+        {
+          user_id: 3,
+          challenge_id: 4,
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("likes", null, {});
+  },
+};
